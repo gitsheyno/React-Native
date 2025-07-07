03fef295316df98e97beb127fd80669b15dd1d12
shayan heidary <hdrydeveloper@gmail.com>
2025-07-07 23:39:16 +0200

    add button - Alert

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Pressable,
+  Alert,
+} from "react-native";
 import { theme } from "./there";
 export default function App() {
+  const handleDelete = () => {
+    Alert.alert("Delete Item", "Are you sure you want to delete this item?", [
+      {
+        text: "Cancel",
+        style: "cancel",
+      },
+      {
+        text: "Delete",
+        onPress: () => console.log("Item deleted"),
+        style: "destructive",
+      },
+    ]);
+  };
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
         <Text style={styles.itemText}>Coffe</Text>
+        <TouchableOpacity style={styles.button} onPress={handleDelete}>
+          <Text style={styles.buttonText}>Delete</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -22,6 +45,20 @@ const styles = StyleSheet.create({
     borderBottomColor: theme.colorCerulian,
     paddingHorizontal: 8,
     paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   itemText: { fontSize: 15, fontWeight: "200" },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  },
 });
