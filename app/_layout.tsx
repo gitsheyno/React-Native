4ccf4b8c21b79dc6006775e14c727ebde599169f
shayan heidary <hdrydeveloper@gmail.com>
2025-07-08 22:07:57 +0200

    modal - top navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,38 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import Feather from "@expo/vector-icons/Feather";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { theme } from "../theme";
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen
         name="index"
         options={{
           title: "Taskly",
-          headerStyle: { backgroundColor: "#1a759f" },
-          headerTintColor: "#fff",
-          headerTitleStyle: { fontWeight: "bold" },
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="home" size={size} color={color} />;
+          },
         }}
       />
-    </Stack>
+      <Tabs.Screen
+        name="counter"
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => {
+            return <AntDesign name="clockcircleo" size={size} color={color} />;
+          },
+        }}
+      />
+      <Tabs.Screen
+        name="idea"
+        options={{
+          title: "Idea",
+          tabBarIcon: ({ color, size }) => {
+            return <FontAwesome5 name="lightbulb" size={size} color={color} />;
+          },
+        }}
+      />
+    </Tabs>
   );
 }
