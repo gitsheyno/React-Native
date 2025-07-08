9d0c0473afcc5a474e6f9c66caa70a908b48c727
shayan heidary <hdrydeveloper@gmail.com>
2025-07-08 23:24:30 +0200

    add controlled input

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,59 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, TextInput } from "react-native";
 import { theme } from "../theme";
 import ShoppingListItem from "../components/ShoppingListItem";
-import { Link } from "expo-router";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+  isCompleted?: boolean;
+};
+
+const initialList: ShoppingListItemType[] = [
+  {
+    id: "1",
+    name: "Coffee",
+    isCompleted: true,
+  },
+  {
+    id: "2",
+    name: "Tea",
+  },
+  {
+    id: "3",
+    name: "Sugar",
+  },
+];
+
 export default function App() {
+  const [value, setValue] = useState("");
+  const [shoppingList, setShoppingList] =
+    useState<ShoppingListItemType[]>(initialList);
+
   return (
     <View style={styles.container}>
-      <Link
-        href="/counter"
-        style={{ textAlign: "center", marginBottom: 10, fontSize: 24 }}
-      >
-        Go to counter
-      </Link>
-      <ShoppingListItem name="Coffe" isCompleted={true} />
-      <ShoppingListItem name="Tea" />
-      <ShoppingListItem name="Sugar" />
+      <TextInput
+        value={value}
+        onChangeText={setValue}
+        placeholder="E.g Codde"
+        style={styles.textInput}
+        returnKeyType="done"
+        onSubmitEditing={() => {
+          console.log("Submitted:", value);
+          setShoppingList((prev) => [
+            { id: Date.now().toString(), name: value, isCompleted: false },
+            ...prev,
+          ]);
+          setValue("");
+        }}
+      />
+      {shoppingList.map((item) => (
+        <ShoppingListItem
+          key={item.id}
+          name={item.name}
+          isCompleted={item.isCompleted}
+        />
+      ))}
     </View>
   );
 }
@@ -22,6 +62,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
+    paddingTop: 12,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 8,
   },
 });
