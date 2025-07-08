d23346cf47f3e8c2a72ba50f8a675b751d1a6d29
shayan heidary <hdrydeveloper@gmail.com>
2025-07-08 23:30:43 +0200

    scroll view

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, TextInput } from "react-native";
+import { StyleSheet, View, TextInput, ScrollView } from "react-native";
 import { theme } from "../theme";
 import ShoppingListItem from "../components/ShoppingListItem";
 import { useState } from "react";
@@ -31,7 +31,11 @@ export default function App() {
     useState<ShoppingListItemType[]>(initialList);
 
   return (
-    <View style={styles.container}>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      stickyHeaderIndices={[0]}
+    >
       <TextInput
         value={value}
         onChangeText={setValue}
@@ -54,7 +58,7 @@ export default function App() {
           isCompleted={item.isCompleted}
         />
       ))}
-    </View>
+    </ScrollView>
   );
 }
 
@@ -62,7 +66,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    paddingTop: 12,
+    padding: 12,
+  },
+  contentContainer: {
+    paddingBottom: 24,
   },
   textInput: {
     borderColor: theme.colorLightGrey,
@@ -72,5 +79,6 @@ const styles = StyleSheet.create({
     marginBottom: 12,
     fontSize: 18,
     borderRadius: 8,
+    backgroundColor: theme.colorWhite,
   },
 });
